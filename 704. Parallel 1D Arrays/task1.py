3f27bff0c5893541e02bd0db61432d1d0db892c2
Ian Simpson <thefamilysimpson@gmail.com>
2024-09-12 21:24:08 +0100

    704

--- 704. Parallel 1D Arrays/task1.py
+++ 704. Parallel 1D Arrays/task1.py
@@ -3,3 +3,7 @@
 name = [ 'Alice' , 'Bob' , 'Carol' , 'David' ]
 print (name[2])
 
+'''
+(a) Which name will be displayed?
+(b) What line of Python code could we use to add ‘Edna’ to the name array?
+'''
